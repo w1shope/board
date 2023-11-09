2d6df32855ea96c638a204e42b2656b023cb8fe2
희망 <122812652+w1shope@users.noreply.github.com>
2023-11-10 02:47:57 +0900

    게시물 등록시 사용자 구분하기 위한 로직 수정

--- src/main/java/com/example/board/service/board/BoardEnrolService.java
+++ src/main/java/com/example/board/service/board/BoardEnrolService.java
@@ -1,5 +1,6 @@
 package com.example.board.service.board;
 
+import com.example.board.domain.user.User;
 import com.example.board.dto.board.BoardEnrolDto;
 import com.example.board.repository.board.BoardEnrolRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class BoardEnrolService {
     private final BoardEnrolRepository boardEnrolRepository;
 
-    public Long enrolBoard(BoardEnrolDto boardEnrolDto) {
-        return boardEnrolRepository.enrolBoard(boardEnrolDto);
+    public Long enrolBoard(BoardEnrolDto boardEnrolDto, User loginUser) {
+        return boardEnrolRepository.enrolBoard(boardEnrolDto, loginUser);
     }
 }
